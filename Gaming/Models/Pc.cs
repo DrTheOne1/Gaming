f7c10fbd2741725e706cc9217131a6e51d725403
Maher <m.r.m.m.h7@gmail.com>
2019-04-17 13:38:05 +0200

    pc class

---
+++ Gaming/Models/Pc.cs
@@ -0,0 +1,16 @@
+﻿namespace Gaming.Models
+{
+    public class Pc
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceDiscount { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl{ get; set; }
+        public bool IsPcOfTheWeek { get; set; }
+    }
+}
